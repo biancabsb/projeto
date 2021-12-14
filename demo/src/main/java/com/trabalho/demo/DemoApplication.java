88ed8b92c05158a651eeae88accf52dd57f7f332
Bianca Barros <barrosbianca972@gmail.com>
2021-12-14 20:28:49 -0300

    trabalho dia 14/12

--- demo/src/main/java/com/trabalho/demo/DemoApplication.java
+++ demo/src/main/java/com/trabalho/demo/DemoApplication.java
@@ -18,6 +18,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 public @SpringBootApplication class DemoApplication {
 
